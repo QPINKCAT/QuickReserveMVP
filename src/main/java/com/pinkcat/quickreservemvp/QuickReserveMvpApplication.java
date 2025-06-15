5d801c8cc4cd3885f55a9bdf70bcc067de9ea1c6
배이경 <yikyung@baeigyeongs-Mac-mini.local>
2025-06-15 13:54:49 +0900

    [init] initiate project

---
+++ src/main/java/com/pinkcat/quickreservemvp/QuickReserveMvpApplication.java
@@ -0,0 +1,13 @@
+package com.pinkcat.quickreservemvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickReserveMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickReserveMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinkcat/quickreservemvp/QuickReserveMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkcat.quickreservemvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickReserveMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

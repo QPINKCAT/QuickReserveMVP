86660cd1fae1ddf9b48f4b5b8a3a655f48b8d8ba
cornchip <yikyung52@naver.com>
2025-06-19 12:08:36 +0900

    [Add] Repository 추가 - Order Item Repository - Order Repository

---
+++ src/main/java/com/pinkcat/quickreservemvp/order/repository/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.pinkcat.quickreservemvp.order.repository;
+
+import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
+import com.pinkcat.quickreservemvp.order.entity.OrderItemEntity;
+
+public interface OrderItemRepository extends ActiveRepository<OrderItemEntity, Long> {
+
+}

038c3937e414e7888e0504a1984e5f1b8a7362d0
cornchip <yikyung52@naver.com>
2025-06-19 20:00:31 +0900

    [Add] User Repository

---
+++ src/main/java/com/pinkcat/quickreservemvp/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.pinkcat.quickreservemvp.user.repository;
+
+import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
+import com.pinkcat.quickreservemvp.user.entity.UserEntity;
+
+public interface UserRepository extends ActiveRepository<UserEntity, Long> {
+
+}

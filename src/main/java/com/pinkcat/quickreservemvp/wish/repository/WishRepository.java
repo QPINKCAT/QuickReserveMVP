58af9edd151072df80fad3e2eb512865592d83eb
cornchip <yikyung52@naver.com>
2025-06-19 20:00:27 +0900

    [Add] Wishlist Repository

---
+++ src/main/java/com/pinkcat/quickreservemvp/wish/repository/WishRepository.java
@@ -0,0 +1,8 @@
+package com.pinkcat.quickreservemvp.wish.repository;
+
+import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
+import com.pinkcat.quickreservemvp.wish.entity.UserProductWishEntity;
+
+public interface WishRepository extends ActiveRepository<UserProductWishEntity, Long> {
+
+}

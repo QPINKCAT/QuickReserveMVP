91f413f043eed275a4507efbb1beadbcbde6d364
cornchip <yikyung52@naver.com>
2025-06-19 20:04:01 +0900

    [Add] Cart Repository

---
+++ src/main/java/com/pinkcat/quickreservemvp/cart/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package com.pinkcat.quickreservemvp.cart.repository;
+
+import com.pinkcat.quickreservemvp.cart.entity.CartEntity;
+import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
+
+public interface CartRepository extends ActiveRepository<CartEntity, Long> {
+
+}

14dae1a699b91b6ffa671234ea58e5b771eb6794
dudgns334 <dudgns334@gmail.com>
2025-06-20 11:20:50 +0900

    [Fix] Exception Handling Error Modified

--- src/main/java/com/pinkcat/quickreservemvp/config/GlobalResponseAdvice.java
+++ src/main/java/com/pinkcat/quickreservemvp/config/GlobalResponseAdvice.java
@@ -1,13 +1,20 @@
 package com.pinkcat.quickreservemvp.config;
 
+import com.pinkcat.quickreservemvp.common.exceptions.PinkCatException;
 import com.pinkcat.quickreservemvp.common.model.BaseResponse;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.LinkedHashMap;
 
 @RestControllerAdvice
 public class GlobalResponseAdvice implements ResponseBodyAdvice<Object> {
@@ -23,9 +30,19 @@ public class GlobalResponseAdvice implements ResponseBodyAdvice<Object> {
             MediaType selectedContentType,
             Class<? extends HttpMessageConverter<?>> selectedConverterType,
             ServerHttpRequest request,
-            ServerHttpResponse response
-    ) {
+            ServerHttpResponse response) {
+        if (body instanceof LinkedHashMap<?, ?>
+                && ((LinkedHashMap<String, ?>) body).containsKey("error")) return body;
+
         if (body instanceof BaseResponse) return body;
+
         return new BaseResponse<>(body);
+    }
+
+    @ExceptionHandler(PinkCatException.class)
+    public ResponseEntity<BaseResponse<String>> handlePinkCatException(
+            PinkCatException ex, WebRequest request) {
+        BaseResponse<String> response = new BaseResponse<>(ex);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }

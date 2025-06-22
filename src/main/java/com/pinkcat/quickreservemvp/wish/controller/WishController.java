26731384045ff7b2744982a8db107ed6ec0adc42
cornchip1 <yikyung52@naver.com>
2025-06-22 23:57:37 +0900

    [Add] 위시리스트 삭제 조회 api 추가

--- src/main/java/com/pinkcat/quickreservemvp/wish/controller/WishController.java
+++ src/main/java/com/pinkcat/quickreservemvp/wish/controller/WishController.java
@@ -1,6 +1,7 @@
 package com.pinkcat.quickreservemvp.wish.controller;
 
 import com.pinkcat.quickreservemvp.common.model.BaseResponse;
+import com.pinkcat.quickreservemvp.wish.dto.DeleteWishlistRequestDTO;
 import com.pinkcat.quickreservemvp.wish.dto.WishlistResponseDTO;
 import com.pinkcat.quickreservemvp.wish.service.WishService;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +12,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/wish")
 @RequiredArgsConstructor
 public class WishController {
-  private final WishService wishService;
+    private final WishService wishService;
 
-  @GetMapping("")
-  @ResponseBody
-  public BaseResponse<WishlistResponseDTO> getWishlist(
-      // authentication 추가 필요
-      @RequestParam(value = "page", defaultValue = "1") int page,
-      @RequestParam(value = "size", defaultValue = "20") int size) {
-    return new BaseResponse<>(wishService.getWishlist(page, size));
-  }
+    @GetMapping("")
+    @ResponseBody
+    public BaseResponse<WishlistResponseDTO> getWishlist(
+        // authentication 추가 필요
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "size", defaultValue = "20") int size) {
+      return new BaseResponse<>(wishService.getWishlist(page, size));
+    }
+
+    @DeleteMapping("")
+    @ResponseBody
+    public BaseResponse<Boolean> delete(@RequestBody DeleteWishlistRequestDTO request) {
+       return new BaseResponse<>(wishService.deleteWishlist(request));
+    }
 }
--- src/main/java/com/pinkcat/quickreservemvp/wish/repository/WishRepository.java
+++ src/main/java/com/pinkcat/quickreservemvp/wish/repository/WishRepository.java
@@ -5,9 +5,12 @@ import com.pinkcat.quickreservemvp.wish.entity.CustomerProductWishEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface WishRepository extends ActiveRepository<CustomerProductWishEntity, Long> {
     Page<CustomerProductWishEntity> findAllByCustomerPk(Long customerPk, Pageable pageable);
 
-    CustomerProductWishEntity findByPk(Long pk);
+    Optional<CustomerProductWishEntity> findByPk(Long pk);
 
+    void delete(CustomerProductWishEntity wish);
 }

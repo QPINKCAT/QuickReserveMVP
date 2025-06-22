b776065c00c1f8cba6a3b06282888ee1ce30cae0
cornchip1 <yikyung52@naver.com>
2025-06-22 23:27:11 +0900

    [Fix] 테이블명Pk -> pk 로 변경함에 따라 메서드명 수정

--- src/main/java/com/pinkcat/quickreservemvp/wish/repository/WishRepository.java
+++ src/main/java/com/pinkcat/quickreservemvp/wish/repository/WishRepository.java
@@ -1,8 +1,13 @@
 package com.pinkcat.quickreservemvp.wish.repository;
 
 import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
-import com.pinkcat.quickreservemvp.wish.entity.UserProductWishEntity;
+import com.pinkcat.quickreservemvp.wish.entity.CustomerProductWishEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface WishRepository extends ActiveRepository<UserProductWishEntity, Long> {
+public interface WishRepository extends ActiveRepository<CustomerProductWishEntity, Long> {
+    Page<CustomerProductWishEntity> findAllByCustomerPk(Long customerPk, Pageable pageable);
+
+    CustomerProductWishEntity findByPk(Long pk);
 
 }

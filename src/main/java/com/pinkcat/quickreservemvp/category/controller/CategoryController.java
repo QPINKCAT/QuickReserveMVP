7140c050d7e990772d23715fdacb1053bde275db
cornchip1 <yikyung52@naver.com>
2025-06-29 22:23:00 +0900

    [Add] 카테고리별 상품 목록 조회 api 추가

--- src/main/java/com/pinkcat/quickreservemvp/category/controller/CategoryController.java
+++ src/main/java/com/pinkcat/quickreservemvp/category/controller/CategoryController.java
@@ -1,12 +1,14 @@
 package com.pinkcat.quickreservemvp.category.controller;
 
 import com.pinkcat.quickreservemvp.category.dto.CategoryListResponseDTO;
+import com.pinkcat.quickreservemvp.category.dto.CategoryProductListResponseDTO;
 import com.pinkcat.quickreservemvp.category.service.CategoryService;
 import com.pinkcat.quickreservemvp.common.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,5 +22,14 @@ public class CategoryController {
     @ResponseBody
     public BaseResponse<CategoryListResponseDTO> getCategories() {
         return new BaseResponse<>(categoryService.getCategories());
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public BaseResponse<CategoryProductListResponseDTO> getCategoryProducts(
+            @RequestParam long categoryId,
+            @RequestParam int page,
+            @RequestParam int size) {
+        return new BaseResponse<>(categoryService.getCategoryProducts(categoryId, page, size));
     }
 }

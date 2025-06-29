bb44c8b36a3a178b1db729afe9b521fd231848c9
cornchip1 <yikyung52@naver.com>
2025-06-29 22:21:44 +0900

    [Add] 상품 controller 추가

---
+++ src/main/java/com/pinkcat/quickreservemvp/product/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.pinkcat.quickreservemvp.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+}

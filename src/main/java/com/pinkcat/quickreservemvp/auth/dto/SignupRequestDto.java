aebe0787173d173b432704e2502f4ff6aba0f0c3
hsilnam <hsilnam3@gmail.com>
2025-07-02 11:27:43 +0900

    refactor: 기존 customer 필드에 대한 커스텀 user Validation 규칙 반영
    - SignupDTO에 Valid 어노테이션 사용
    - CustomerEntity에 ValidationPatterns 사용

--- src/main/java/com/pinkcat/quickreservemvp/auth/dto/SignupRequestDto.java
+++ src/main/java/com/pinkcat/quickreservemvp/auth/dto/SignupRequestDto.java
@@ -1,6 +1,11 @@
 package com.pinkcat.quickreservemvp.auth.dto;
 
 import com.pinkcat.quickreservemvp.common.enums.GenderEnum;
+import com.pinkcat.quickreservemvp.common.validation.ValidationPatterns;
+import com.pinkcat.quickreservemvp.common.validation.user.EmailValid;
+import com.pinkcat.quickreservemvp.common.validation.user.NameValid;
+import com.pinkcat.quickreservemvp.common.validation.user.PasswordValid;
+import com.pinkcat.quickreservemvp.common.validation.user.PhoneNumberValid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -13,19 +18,17 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignupRequestDto {
 
-  @NotBlank private String id;
-
-  @NotBlank private String name;
-
   @NotBlank
-  @Size(max = 20, message = "비밀번호는 최대 20자까지 가능합니다.")
-  private String password;
+  @Pattern(regexp = ValidationPatterns.ID, message = "{user.id.invalid}")
+  private String id;
 
-  @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "올바른 핸드폰 번호 형식이 아닙니다.")
-  private String phoneNumber;
+  @NotBlank @NameValid private String name;
 
-  @Email(message = "올바른 이메일 주소 형식이 아닙니다.")
-  private String email;
+  @NotBlank @PasswordValid private String password;
+
+  @NotBlank @PhoneNumberValid private String phoneNumber;
+
+  @NotBlank @EmailValid private String email;
 
   private GenderEnum gender;
 }

c83489c1921fe32b60b04d2735365f0154ff6f08
cornchip1 <yikyung52@naver.com>
2025-07-06 23:35:58 +0900

    [Add] 상품별 이미지 조회 메서드 추가

--- src/main/java/com/pinkcat/quickreservemvp/product/repository/ProductImageRepository.java
+++ src/main/java/com/pinkcat/quickreservemvp/product/repository/ProductImageRepository.java
@@ -1,10 +1,12 @@
 package com.pinkcat.quickreservemvp.product.repository;
 
 import com.pinkcat.quickreservemvp.common.repository.ActiveRepository;
+import com.pinkcat.quickreservemvp.product.entity.ProductEntity;
 import com.pinkcat.quickreservemvp.product.entity.ProductImageEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductImageRepository extends ActiveRepository<ProductImageEntity, Long> {
@@ -15,4 +17,10 @@ public interface ProductImageRepository extends ActiveRepository<ProductImageEnt
             "   i.product.pk = :productPk" +
             "   and i.order = 1")
     Optional<String> findThumbnailByProductPk(@Param("productPk") Long productPk);
+
+    @Query("select i " +
+            "from ProductImageEntity i " +
+            "where i.product = :product " +
+            "order by i.order asc")
+    List<ProductImageEntity> findAllImagesByProduct(@Param("product") ProductEntity product);
 }

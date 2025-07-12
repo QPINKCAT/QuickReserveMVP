4ada29e844550e080a440fb0816e719fc42e9d0c
cornchip1 <yikyung52@naver.com>
2025-07-13 02:23:33 +0900

    [Fix] 결제 시 주문 아이템이 생성되지 않는 문제 해결

--- src/main/java/com/pinkcat/quickreservemvp/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/pinkcat/quickreservemvp/payment/service/PaymentServiceImpl.java
@@ -1,11 +1,14 @@
 package com.pinkcat.quickreservemvp.payment.service;
 
+import com.pinkcat.quickreservemvp.common.enums.OrderStatusEnum;
 import com.pinkcat.quickreservemvp.common.enums.PaymentStatusEnum;
 import com.pinkcat.quickreservemvp.common.exceptions.ErrorMessageCode;
 import com.pinkcat.quickreservemvp.common.exceptions.PinkCatException;
 import com.pinkcat.quickreservemvp.customer.entity.CustomerEntity;
 import com.pinkcat.quickreservemvp.customer.repository.CustomerRepository;
 import com.pinkcat.quickreservemvp.order.entity.OrderEntity;
+import com.pinkcat.quickreservemvp.order.entity.OrderItemEntity;
+import com.pinkcat.quickreservemvp.order.repository.OrderItemRepository;
 import com.pinkcat.quickreservemvp.order.repository.OrderRepository;
 import com.pinkcat.quickreservemvp.payment.dto.PaymentRequestDTO;
 import com.pinkcat.quickreservemvp.payment.dto.PaymentRequestDTO.Item;
@@ -33,6 +36,7 @@ public class PaymentServiceImpl implements PaymentService {
     private final ProductRepository productRepository;
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
 
     @Override
     @Transactional
@@ -41,6 +45,7 @@ public class PaymentServiceImpl implements PaymentService {
                 new PinkCatException("비활성화된 계정입니다. 관리자에게 문의해주세요.", ErrorMessageCode.CUSTOMER_INACTIVE));
 
         List<Item> items = request.getItems();
+
         items.forEach(item -> {
             ProductEntity product = productRepository.findByPk(item.getProductId()).orElseThrow(() ->
                     new PinkCatException("존재하지 않는 상품입니다", ErrorMessageCode.NO_SUCH_PRODUCT));
@@ -55,11 +60,6 @@ public class PaymentServiceImpl implements PaymentService {
         });
 
         // [2] 정상 처리 시
-        // 장바구니 아이템 목록에서 결제된 아이템을 삭제한다
-        for (Item item : items) {
-            customerRepository.deleteById(item.getCartItemId());
-        }
-
         // 주문번호를 채번한다
         String orderNumber = generateOrderNumber();
 
@@ -68,6 +68,25 @@ public class PaymentServiceImpl implements PaymentService {
                 .customer(customer)
                 .orderNum(orderNumber)
                 .build());
+
+        for (Item item : items) {
+            // 주문 아이템을 등록한다
+            ProductEntity product = productRepository.findByPk(item.getProductId()).orElseThrow(() ->
+                    new PinkCatException("존재하지 않는 상품입니다", ErrorMessageCode.NO_SUCH_PRODUCT));
+
+            orderItemRepository.save(
+                OrderItemEntity.builder()
+                    .order(order)
+                    .product(product)
+                    .originalPrice(product.getPrice())
+                    .status(OrderStatusEnum.COMPLETED)
+                    .saledPrice(item.getDiscountPrice())
+                    .quantity(item.getQuantity())
+                    .build());
+
+            // 장바구니 아이템 목록에서 결제된 아이템을 삭제한다
+            customerRepository.deleteById(item.getCartItemId());
+        }
 
         // 결제 내역 저장
         paymentRepository.save(PaymentEntity.builder()
